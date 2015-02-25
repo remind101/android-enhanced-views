2ac5b1b21e02a5cc0c7e6ccad7e062ace9409f01
max <max@remind101.com>
2015-02-24 14:07:18 -1000

    added gravity parameter to circle indicator view

--- src/main/java/com/remind101/android/views/CircleIndicatorView.java
+++ src/main/java/com/remind101/android/views/CircleIndicatorView.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 
 import com.remind101.android.enhancedviews.R;
@@ -26,12 +27,12 @@ public class CircleIndicatorView extends View {
     private float radius;
     private Paint[] activePaint;
     private Paint inactivePaint;
+    private int gravity;
 
     public CircleIndicatorView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(attrs);
     }
-
 
     private void init(AttributeSet attrs) {
         TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.CircleIndicatorView, 0, 0);
@@ -52,6 +53,7 @@ public class CircleIndicatorView extends View {
                 paint.setColor(color);
                 activePaint[i] = paint;
             }
+            gravity = a.getInt(R.styleable.CircleIndicatorView_gravity, Gravity.NO_GRAVITY);
             activeDrawable = a.getDrawable(R.styleable.CircleIndicatorView_activeDrawable);
             if (activeDrawable != null) {
                 activeDrawableWidth = activeDrawable.getIntrinsicWidth();
@@ -159,32 +161,51 @@ public class CircleIndicatorView extends View {
 
         int i;
         int left, top, right, bottom;
+        int paddingTop = getPaddingTop();
+        int paddingLeft = getPaddingLeft();
+
+        switch (gravity) {
+            case Gravity.TOP:
+                paddingTop = 0;
+                break;
+            case Gravity.CENTER:
+                paddingTop = (int) (getHeight() / 2 - (radius <= 0 ?
+                        Math.max(activeDrawableHeight, inActiveDrawableHeight) / 2 :
+                        radius));
+                paddingLeft = (int) (getWidth() / 2 - (radius <= 0 ?
+                        ((pageCount - 1) * (inActiveDrawableWidth + drawablePadding) + activeDrawableWidth) / 2 :
+                        (2 * radius * pageCount + (pageCount - 1) * drawablePadding) / 2));
+                break;
+            case Gravity.BOTTOM:
+                paddingTop = (int) (getHeight() - (radius <= 0 ? Math.max(activeDrawableHeight, inActiveDrawableHeight) : 2 * radius));
+                break;
+        }
 
         for (i = 0; i < pageCount; i++) {
             if (i == currentActive) {
                 if (radius <= 0) {
-                    left = getPaddingLeft() + (i * activeDrawableWidth) + (i * drawablePadding);
-                    top = getPaddingTop();
+                    left = paddingLeft + (i * activeDrawableWidth) + (i * drawablePadding);
+                    top = paddingTop;
                     right = left + activeDrawableWidth;
                     bottom = activeDrawableHeight + top;
                     activeDrawable.setBounds(left, top, right, bottom);
                     activeDrawable.draw(canvas);
                 } else {
-                    float cx = getPaddingRight() + radius + (2 * radius * i) + (drawablePadding * i);
-                    float cy = getPaddingTop() + radius;
+                    float cx = paddingLeft + radius + (2 * radius * i) + (drawablePadding * i);
+                    float cy = paddingTop + radius;
                     canvas.drawCircle(cx, cy, radius, activePaint[i - activePaint.length * (i / activePaint.length)]);
                 }
             } else {
                 if (radius <= 0) {
-                    left = getPaddingLeft() + (i * inActiveDrawableWidth) + (i * drawablePadding);
-                    top = getPaddingTop();
+                    left = paddingLeft + (i * inActiveDrawableWidth) + (i * drawablePadding);
+                    top = paddingTop;
                     right = left + inActiveDrawableWidth;
                     bottom = inActiveDrawableHeight + top;
                     inactiveDrawable.setBounds(left, top, right, bottom);
                     inactiveDrawable.draw(canvas);
                 } else {
-                    float cx = getPaddingRight() + radius + (2 * radius * i) + (drawablePadding * i);
-                    float cy = getPaddingTop() + radius;
+                    float cx = paddingLeft + radius + (2 * radius * i) + (drawablePadding * i);
+                    float cy = paddingTop + radius;
                     canvas.drawCircle(cx, cy, radius, inactivePaint);
                 }
             }
