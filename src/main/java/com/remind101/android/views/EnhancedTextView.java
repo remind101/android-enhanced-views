88af1af0889362b956f0493cdb47c412c11f6a21
Nathan Barraille <nathan.barraille@gmail.com>
2015-07-08 10:23:34 -0700

    Fixing click on left drawable as well

--- src/main/java/com/remind101/android/views/EnhancedTextView.java
+++ src/main/java/com/remind101/android/views/EnhancedTextView.java
@@ -611,16 +611,18 @@ public class EnhancedTextView extends TextView {
         switch (which) {
             case LEFT:
                 if (isDrawableSticky) {
+                    // TODO: This doesn't work
                     rect.left = getScrollX() + (getWidth() - dr.getIntrinsicWidth()
                             - getCompoundDrawablePadding() - getMaxLineWidth()) / 2;
                 } else {
-                    rect.left = getScrollX() + getPaddingLeft();
+                    rect.left = getPaddingLeft();
                 }
                 rect.top = getScrollY() + getCompoundPaddingTop()
                         + (vspace - dr.getIntrinsicHeight()) / 2;
                 break;
             case RIGHT:
                 if (isDrawableSticky) {
+                    // TODO: This doesn't work
                     rect.left = getScrollX() + (getWidth() + getCompoundDrawablePadding()
                             + getMaxLineWidth() - dr.getIntrinsicWidth()) / 2;
                 } else {
